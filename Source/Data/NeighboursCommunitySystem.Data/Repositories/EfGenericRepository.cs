5b58cd7c7174764522effd74bad1f955ddfda9a3
InKolev <in.kolev.b@gmail.com>
2015-11-18 02:22:33 +0200

    Extracted DbException in CustomException class.

--- Source/Data/NeighboursCommunitySystem.Data/Repositories/EfGenericRepository.cs
+++ Source/Data/NeighboursCommunitySystem.Data/Repositories/EfGenericRepository.cs
@@ -5,7 +5,7 @@
     using System.Linq;
     using DbContexts;
     using System.Data.Entity.Validation;
-    using System.Text;
+    using CustomExceptions;
 
     public class EfGenericRepository<T> : IRepository<T>
         where T : class
@@ -112,46 +112,6 @@
             {
                 var newException = new FormattedDbEntityValidationException(e);
                 throw newException;
-            }
-        }
-
-        public class FormattedDbEntityValidationException : Exception
-        {
-            public FormattedDbEntityValidationException(DbEntityValidationException innerException) :
-                base(null, innerException)
-            {
-            }
-
-            public override string Message
-            {
-                get
-                {
-                    var innerException = InnerException as DbEntityValidationException;
-                    if (innerException != null)
-                    {
-                        StringBuilder sb = new StringBuilder();
-
-                        sb.AppendLine();
-                        sb.AppendLine();
-                        foreach (var eve in innerException.EntityValidationErrors)
-                        {
-                            sb.AppendLine(string.Format("- Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                                eve.Entry.Entity.GetType().FullName, eve.Entry.State));
-                            foreach (var ve in eve.ValidationErrors)
-                            {
-                                sb.AppendLine(string.Format("-- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
-                                    ve.PropertyName,
-                                    eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
-                                    ve.ErrorMessage));
-                            }
-                        }
-                        sb.AppendLine();
-
-                        return sb.ToString();
-                    }
-
-                    return base.Message;
-                }
             }
         }
     }
