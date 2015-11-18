ba778155950ee4c4a1c8697b1c40b1fb618afa34
mdraganov <mariyandraganov@gmail.com>
2015-11-19 01:17:37 +0200

    Added route tests project.

---
+++ Source/Tests/NeighboursCommunitySystem.Api.Tests/RouteTests/TaxesControllerRouteTests.cs
@@ -0,0 +1,29 @@
+﻿namespace NeighboursCommunitySystem.Api.Tests.RouteTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Web.Http;
+    using API;
+    using MyTested.WebApi;
+    using API.Controllers;
+
+    [TestClass]
+    public class TaxesControllerRouteTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            MyWebApi.IsUsing(config);
+        }
+        
+        //[TestMethod]
+        //public void GetWithIdShouldMapCorrectly()
+        //{
+        //    MyWebApi
+        //        .Routes()
+        //        .ShouldMap("api/taxes/get/1")
+        //        .To<TaxesController>(c => c.Get(1));
+        //}
+    }
+}
