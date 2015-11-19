cbab984e3ab923cc7e527839b9dbcb6c6b9a0eb5
mdraganov <mariyandraganov@gmail.com>
2015-11-19 22:10:49 +0200

    Updated postByAdmin endpoint.

--- Source/Server/NeighboursCommunitySystem.API/Controllers/CommunitiesController.cs
+++ Source/Server/NeighboursCommunitySystem.API/Controllers/CommunitiesController.cs
@@ -75,24 +75,19 @@
         }
 
         [HttpPost]
-        [Authorize]
-        public IHttpActionResult PostCommunityByLoggedAdmin([FromBody]CommunityDataTransferModel model)
+        [ValidateModel]
+        [Authorize(Roles = "Administrator")]
+        public IHttpActionResult PostCommunityByLoggedAdmin(CommunityDataTransferModel model)
         {
             // Check if community with the same name already exists.
             if (this.communities.All().Any(c => c.Name == model.Name))
             {
-                return this.Ok("Community model with the same name already exists.");
+                return this.BadRequest("Community model with the same name already exists.");
             }
 
-            if (ModelState.IsValid)
-            {
-                var newCommunityId = communities.Add(model);
-                return this.Ok(newCommunityId);
-            }
-            else
-            {
-                return this.BadRequest("not valid model input");
-            }              
+            var newCommunityId = communities.Add(model);
+
+            return this.Ok(newCommunityId);
         }
 
 
