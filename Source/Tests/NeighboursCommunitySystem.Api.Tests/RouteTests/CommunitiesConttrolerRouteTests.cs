5bb81fecdc6c373b200f4e3abe8a93aa97706a46
mdraganov <mariyandraganov@gmail.com>
2015-11-20 02:03:01 +0200

    Added assembly initializer for the test assembly and CommunitiesConttrolerRouteTests.cs

---
+++ Source/Tests/NeighboursCommunitySystem.Api.Tests/RouteTests/CommunitiesConttrolerRouteTests.cs
@@ -0,0 +1,19 @@
+﻿namespace NeighboursCommunitySystem.Api.Tests.RouteTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MyTested.WebApi;
+    using API.Controllers;
+
+    [TestClass]
+    public class CommunitiesConttrolerRouteTests
+    {
+        [TestMethod]
+        public void GetShouldMapCorrectly()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/communities")
+                .To<CommunitiesController>(c => c.Get());
+        }
+    }
+}
---
+++ Source/Tests/NeighboursCommunitySystem.Api.Tests/TestsInitializer.cs
@@ -0,0 +1,19 @@
+﻿namespace NeighboursCommunitySystem.Api.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Web.Http;
+    using API;
+    using MyTested.WebApi;
+
+    [TestClass]
+    public class TestsInitializer
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            MyWebApi.IsUsing(config);
+        }
+    }
+}
